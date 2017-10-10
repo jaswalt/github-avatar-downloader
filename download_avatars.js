088bf4f22d436c2b95a36e004dab6b602ae2201b
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-10 04:22:32 +0000

    defines new github user and token variables and able to request url

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,7 @@
 var request = require('request');
+var GITHUB_USER = "jaswalt";
+var GITHUB_TOKEN = "24ceafb8196410add9a17479e722b322bc3ea0cb";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
